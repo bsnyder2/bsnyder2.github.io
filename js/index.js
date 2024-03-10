9c22df32343da4b70471f75e6a7c4fe3a081d0ef
Ben Snyder <bsnyder2@users.noreply.github.com>
2024-03-10 05:26:22 -0400

    Redesign and scroll1 mostly complete

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ Array.from(projectLinks).forEach((projectLink, i) => {
 
 // This will eventually not use inline styling because that is bad (change image src attribute)
 // Can I do this without IDs? For the whole projectlink class
+
+globe = document.getElementById("globe");
+console.log(globe);
+globe.addEventListener("click", () => {
+  console.log("scroll fades in");
+  scroll.style.visibility = "visible";
+  scroll.style.animation = "fadeinout 2s linear";
+});
+
+// Click daily scroll to enlarge
+scroll = document.getElementById("scroll");
+scroll.style.visibility = "hidden";
+
+console.log(scroll);
+
+// scroll.style.visibility = "visible";
